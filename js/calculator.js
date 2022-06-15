ec48385479d3010bae766b32d0919d82b5d1aaa9
SimonO7 <sdragonz461@gmail.com>
2022-06-14 18:07:27 -0700

    Fix main calculator functions
    Fix the storenum function to check if both operands are there to operate
    Remove operandTwo variable as the second operand will go directly on
    the operate function.
    Add function where after operator is pressed, the screen is cleared to allow
    new entry.
    Add functionality for equal button

--- js/calculator.js
+++ js/calculator.js
@@ -1,8 +1,10 @@
 const display = document.querySelector(".display");
 
-let operandOne = 0;
-let operandTwo = 0; 
-let operation = 0;
+let operandOne = 1;
+let operation = 1;
+
+let waitingForNewInput = false;
+let waitingForSecondOperand = false;
 
 function add(operandOne, operandTwo) {
     return operandOne + operandTwo;
@@ -43,21 +45,49 @@ function operate(operandOne, operandTwo, operator) {
 }
 
 function displayNumber(event) {
-    display.textContent += event.target.id;
+    if (waitingForNewInput) {
+        display.textContent = event.target.id;
+        waitingForNewInput = false;
+    }
+    else {
+        display.textContent += event.target.id;
+    }
 }
 
 function clearDisplay() {
     display.textContent = "";
+    operandOne = 1; 
+    operation = 1;
+}
+
+function storeNum(event) {
+    if (waitingForSecondOperand) {
+        operandOne = operate(operandOne, Number(display.textContent), operation);
+        operation = event.target.id;
+        display.textContent = operandOne;
+    }
+    else {
+        operandOne = Number(display.textContent);
+        operation = event.target.id;
+        waitingForSecondOperand = true;
+    }
+    waitingForNewInput = true;
 }
 
 function main() {
     const digitsButtons = document.querySelectorAll(".digits");
     const clearButton = document.querySelector("#clearDisplay");
     const operators = document.querySelectorAll(".operators");
+    const equalBtn = document.querySelector("#equal");
 
     digitsButtons.forEach((button) => button.addEventListener("click", displayNumber));
     clearButton.addEventListener("click", clearDisplay);
-    operators.forEach((button) => button.addEventListener("click", operateNum));
+    operators.forEach((button) => button.addEventListener("click", storeNum));
+    equalBtn.addEventListener("click", () => {
+        display.textContent = operate(operandOne, Number(display.textContent), operation);
+        waitingForNewInput = true;
+        waitingForSecondOperand = false;
+    });
 }
 
 main();
