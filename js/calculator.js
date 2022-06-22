cb673ddd6c41452b31482518e1aa04c3513a61e8
SimonO7 <sdragonz461@gmail.com>
2022-06-21 20:19:29 -0700

    Add documentation for each function

--- js/calculator.js
+++ js/calculator.js
@@ -1,16 +1,19 @@
+//JS file for calculator project
+
 const display = document.querySelector(".display");
 const intermediateStepsDisplay = document.querySelector(".intStepsDisplay");
 
+//Flags that will keep track of the calculator's operation. These are the default values.
 display.textContent = "0";
 let operandOne = 1;
 let operation = "none";
+let waitingForNewInput = true;      //Tracks if calculator is waiting for new input.
+let waitingForOperandTwo = false;   //Tracks if calculator is waiting for the second operand. (ie. it's in between operations)
+let decimalAvailable = true;        //Tracks if the decimal button is available to be entered on the current operand.
+let digitsEntered = false;          //Tracks if new digits have been entered in between operations.
+let equalBtnPressed = false;        //Tracks if the equal button has been pressed in the current calculation.
 
-let waitingForNewInput = true;
-let waitingForOperandTwo = false;
-let decimalAvailable = true;
-let digitsEntered = false;
-let equalBtnPressed = false;
-
+//The basic four operations: add, subtract, multiply, and divide.
 function add(operandOne, operandTwo) {
     return operandOne + operandTwo;
 }
@@ -31,8 +34,11 @@ function divide(operandOne, operandTwo) {
 }
 
 function operate(operandOne, operandTwo, operator) {
-    //inputs operandOne and operandTwo are numbers. Operator is a string.
-    //Output is number
+    //Perform an operation on two operands.
+    //Parameters: operandOne, Number: the first operand.
+    //            operandTwo, Number: the second operand.
+    //            operator, string: the operation to perform.
+    //Returns: number, the result of the calculation.
     let result;
     switch(operator) {
         case "add":
@@ -56,13 +62,16 @@ function operate(operandOne, operandTwo, operator) {
 }
 
 function displayNumber(number) {
-    //Input: number: type string. The number/digit to display
-    //What to do when a digit button is pressed
-    //If waiting for new input, clear display and show digits instead of appending to old string
+    //Define what to do when a digit button is pressed.
+    //Parameters: number, string: The digit to display.
+    //Returns: None. It will modify the display directly.
+
+    //Of equal button was pressed, clear the intermediate steps display as well, and reset the flag.
     if (equalBtnPressed) {
         intermediateStepsDisplay.textContent = ""
         equalBtnPressed = false;
     }
+    //If waiting for new input, clear the display and show digits instead of appending to existing display values.
     if (waitingForNewInput || display.textContent === "0") {
         display.textContent = number;
         waitingForNewInput = false;
@@ -75,12 +84,16 @@ function displayNumber(number) {
 
 function clearCurrentEntry() {
     //Clear the displays
+    //Parameters: None
+    //Returns: None. It will directly modify the displays.
     display.textContent = "0";
     intermediateStepsDisplay.textContent = ""
 }
 
 function clearFlags() {
-    //Clear the flags
+    //Reset the flags to default values.
+    //Parameters: None
+    //Returns: None
     operandOne = 0; 
     operation = "none";
     decimalAvailable = true;
@@ -90,8 +103,10 @@ function clearFlags() {
 }
 
 function storeNum(operatorPressed) {
-    //Input: operatorPressed: type String. The operator pressed.
-    //What to do when a operator button is pressed
+    //Define what to do when an operator button (add, subtract, multiply, divide button) is pressed.
+    //Parameter: operatorPressed, string: the operator pressed, represented in either words or the symbol.
+    //Returns: None.
+
     if (waitingForOperandTwo && digitsEntered) {
         operandOne = operate(operandOne, Number(display.textContent), operation);
         display.textContent = operandOne;
@@ -104,16 +119,18 @@ function storeNum(operatorPressed) {
     }
     intermediateStepsDisplay.textContent = `${operandOne} ${convertOperatorToSymbol(operation)}`;
     waitingForNewInput = true;
-    decimalAvailable = true;    //Reactivate decimal button
-    digitsEntered = false;      //Tell program it's now waiting for new digits to be entered
+    decimalAvailable = true;    //Reactivate decimal button.
+    digitsEntered = false;      //Tell program it's now waiting for new digits to be entered.
     equalBtnPressed = false;    //Have to set to allow chaining operations to be displayed
 }
 
 function addDecimal() {
-    //check if decimal btn is active for current operand.
-    //if so, add decimal to current display string.
+    //Check if decimal button is active for current operand.
+    //Decimal button is active if the current operand does not already have a decimal.
+    //Parameters: None
+    //Returns: None
     if (decimalAvailable && waitingForNewInput) {
-        display.textContent = "0."  //If decimal is pressed before other number, add a zero before it to avoid bug with interpreting.
+        display.textContent = "0."  //If decimal is pressed before other number, add a zero before it.
         waitingForNewInput = false;
         if (equalBtnPressed) {
             intermediateStepsDisplay.textContent = "";
@@ -126,8 +143,10 @@ function addDecimal() {
 }
 
 function equalSignCompute() {
-    //When equal sign is pressed, check if it's waiting for operand two or not, as well as if a new digit
-    //has been entered. If so, calculate and display result, then reset the flags.
+    //What to do when the equal sign is pressed.
+    //When equal sign is pressed, check if it's waiting for operand two or not, as well as if a new digit has been entered. If so, calculate and display result, then reset the flags.
+    //Parameters: None
+    //Returns: None
     if (waitingForOperandTwo && digitsEntered) {
         let operandTwo = Number(display.textContent);
         display.textContent = operate(operandOne, operandTwo, operation);
@@ -139,8 +158,10 @@ function equalSignCompute() {
 
 function toggleNegativeSign() {
     //Toggle the negative sign.
-    //If number is already negative, remove the negative symbol
-    //If number is postive, add negative symbol.
+    //If number is already negative, remove the negative symbol from beginning of number.
+    //If number is postive, add negative symbol in front of number.
+    //Parameters: None
+    //Returns: None
     if (Number(display.textContent) < 0) {
         display.textContent = display.textContent.slice(1);
     }
@@ -150,10 +171,11 @@ function toggleNegativeSign() {
 }
 
 function deleteLastDigit() {
-    //For backspace button. Remove the last digit entered.
-    //Backspace button only works when not waiting for new input 
-    //(ie. cannot delete a digit after a operator is pressed)
+    //What to do when backspace button is pressed. Remove the last digit entered.
+    //Backspace button only works when not waiting for new input (ie. cannot delete a digit after a operator is pressed).
     //If only 1 digit left on display when backspace is pressed, just clear the display.
+    //Parameters: None
+    //Returns: None
     if (!(waitingForNewInput)) {
         if (display.textContent.length === 1) {
             display.textContent = "0";  //Only clear primary display. Don't clear intermediate steps display.
@@ -169,14 +191,17 @@ function deleteLastDigit() {
 }
 
 function percentage() {
+    //What to do when percent button is pressed.
+    //Calculate the decimal equivalent of the percent of the currently input number.
+    //Parameters: None
+    //Returns: None
     display.textContent = Number(display.textContent)/100;
 }
 
 function convertOperatorToSymbol(operation) {
-    //Convert operator symbols from its name to the correcy symbol
-    //for display on the intermediate step display
-    //Input: symbol, string: the word of the operation to convert to symbol
-    //Returns: string, the symbol to represent the operation
+    //Convert operator symbols from its name to the correct symbol for display on the intermediate step display
+    //Parameter: operation, string: the word of the operation to convert to symbol, or the symbol of the operation.
+    //Returns: string, the symbol to represent the operation on the display.
     switch(operation) {
         case "add":
         case "+":
@@ -194,7 +219,10 @@ function convertOperatorToSymbol(operation) {
 }
 
 function main() {
-    //Select the buttons and add the appropriate event listeners
+    //Main function to run on page load.
+    //Select the buttons and add the appropriate event listeners.
+    //Parameters: None
+    //Returns: None
     const digitsBtns = document.querySelectorAll(".digits");
     const clearAllBtn = document.querySelector("#clearAll");
     const operators = document.querySelectorAll(".operators");
@@ -221,8 +249,11 @@ function main() {
 }
 
 function keyboardListener(event) {
+    //Defines how to handle key presses on the keyboard.
+    //Parameters: event, Event: the keyDown event generated when a keyboard key is pressed.
+    //Returns: None
     const keyPressed = event.key;
-    //Number buttons
+    //Number buttons. Check if the key pressed is a number.
     if (Number.isInteger(Number(keyPressed)) && keyPressed !== " ") {
         displayNumber(keyPressed);
     }
@@ -235,7 +266,7 @@ function keyboardListener(event) {
             case "/":
                 storeNum(keyPressed);                    
                 break;
-                //Decimal button
+            //Decimal button
             case ".":
                 addDecimal();
                 break;
